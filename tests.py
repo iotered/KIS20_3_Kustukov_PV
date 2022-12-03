a9c33674c32ff43274ea7b00268ca47ad23a38ed
Кустюков Павел <uetk-github@rijndael.cc>
2022-12-03 12:27:32 +0300

    Fix tests

--- tests.py
+++ tests.py
@@ -3,23 +3,23 @@ from main import count_digits
 
 
 class TestCountDigits(unittest.TestCase):
-    def wrong_type(self):
+    def test_wrong_type(self):
         with self.assertRaises(AssertionError):
             count_digits(42)
 
-    def str_subclasses(self):
+    def test_str_subclasses(self):
         class TestStr(str):
             pass
         line = TestStr("The answer is 42")
         self.assertEqual(count_digits(line), 2)
 
-    def no_digits(self):
+    def test_no_digits(self):
         self.assertEqual(count_digits("The answer is..."), 0)
 
-    def all_digits(self):
+    def test_all_digits(self):
         self.assertEqual(count_digits("1234567890"), 10)
 
-    def some_digits(self):
+    def test_some_digits(self):
         self.assertEqual(count_digits("The answer is 42"), 2)
 
 
