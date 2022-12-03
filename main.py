108831fbddf6677f0764c09d88365b0d8e0818ca
Кустюков Павел <uetk-github@rijndael.cc>
2022-12-03 12:19:43 +0300

    Document count_digits function

--- main.py
+++ main.py
@@ -2,6 +2,12 @@ import string
 
 
 def count_digits(line: str) -> int:
+    """
+    Counts number of ASCII-digits in line
+
+    :param line: string to count digits in
+    :return: number of digits
+    """
     assert isinstance(line, str)
     total = 0
     for char in line:
